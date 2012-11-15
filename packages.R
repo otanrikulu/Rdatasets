2be7bef138543d24d485652ac920d0b1ef6976b9
Vincent Arel-Bundock <varel@umich.edu>
2012-11-14 22:55:51 -0500

    package subfolders

--- packages.R
+++
@@ -1,5 +0,0 @@
-c("datasets", "Amelia", "boot", "car", "cluster", "COUNT", "countrycode", 
-"Ecdat", "gam", "gap", "ggplot2", "HistData", "lattice", "lubridate", 
-"MASS", "mgcv", "nlme", "nlreg", "plm", "plyr", "pscl", "reshape2", 
-"rpart", "sandwich", "sem", "statmod", "strucchange", "survival", 
-"TTR", "vcd", "xtable", "xts", "Zelig")
