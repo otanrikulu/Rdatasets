7d1353687b935ef96f77b74a94024d5258309d65
Vincent Arel-Bundock <varel@umich.edu>
2012-11-16 14:32:34 -0500

    add robustbase

--- Rdatasets.R
+++ Rdatasets.R
@@ -1,81 +1,52 @@
 library(R2HTML)
 
-packages = dget('packages.R')
+packages = c("datasets", "boot", "robustbase", "car", "cluster", "COUNT", "Ecdat", "gap", "ggplot2", "HistData", "lattice", "MASS", "nlreg", "plm", "plyr", "pscl", "reshape2", "rpart", "sandwich", "sem",  "survival", "vcd", "Zelig")
 #install.packages(packages)
-
-# Destinations
-path_html = 'doc/'
-path_csv = 'csv/'
-
-# Get list of available datasets (This list will change depending on which
-# packages the user has installed locally)
-index = data(package=.packages(all.available = TRUE))$results[,c(1,3,4)]
+index = data(package=packages)$results[,c(1,3,4)]
 index = data.frame(index, stringsAsFactors=FALSE)
 index_out = NULL
 
 # Load packages which store datasets 
-packages = unique(index[,'Package'])
 for (i in packages) {
         library(i, character.only=TRUE)
 }
 
-# Remove datasets with duplicated names
-#dup = duplicated(tolower(index$Item))
-dup = duplicated(index$Item)
-index = index[!dup,]
-
+# Save datasets
 for (i in 1:nrow(index)) {
-	dataset = index$Item[i]
-	package = index$Package[i]
-	# Load data in new environment (very hackish) 
+    dataset = index$Item[i]
+    package = index$Package[i]
+    # Load data in new environment (very hackish) 
     e = new.env(hash = TRUE, parent = parent.frame(), size = 29L)
     cmd = paste('data(', dataset, ', envir=e)', sep='')
     eval(parse(text=cmd))
     d = e[[dataset]]
     if(class(d) %in% c('data.frame', 'matrix', 'numeric', 'table')){
         cat("Processing data set: ", dataset, "\n")
-        dest_csv = paste(path_csv, dataset, '.csv', sep='')
-        dest_html = paste(path_html, dataset, '.html', sep='')
+        try(dir.create(paste('csv/', package, sep='')))
+        try(dir.create(paste('doc/', package, sep='')))
+        dest_csv = paste('csv/', package, '/', dataset, '.csv', sep='')
+        dest_doc = paste('doc/', package, '/', dataset, '.html', sep='')
         # Save data as CSV
         write.csv(d, dest_csv)
         # Save documentation as HTML
         help.ref = help(eval(dataset), package=eval(package))
         help.file = utils:::.getHelpFile(help.ref)
-        tools::Rd2HTML(help.file, out=dest_html)
+        tools::Rd2HTML(help.file, out=dest_doc)
         # Add entry to index out
         index_out = rbind(index_out, index[i,]) 
     }
 }
 
-# Make data_index.html
-make_link = function(dataset, folder){
-    ext = ifelse(folder=='doc', '.html', '.csv')
-    link = paste(folder, '/', dataset, ext, sep='')
-    out = paste("<a href=", link, ">", folder, "</a>", sep='')
-    return(out)
-}
-datasets = index_out[,'Item']
-links_doc = sapply(datasets, make_link, 'doc')
-links_csv = sapply(datasets, make_link, 'csv')
-out = cbind(index_out, links_doc, links_csv)
-row.names(out) = NULL
-colnames(out) = c('R Package', 'Dataset', 'Description', 'Doc', 'CSV')
+# CSV index 
+index_out$csv = paste('https://raw.github.com/vincentarelbundock/Rdatasets/master/csv/', 
+                      index_out$Package, '/', index_out$Item, '.csv', sep='')
+index_out$doc = paste('https://raw.github.com/vincentarelbundock/Rdatasets/master/doc/', 
+                      index_out$Package, '/', index_out$Item, '.html', sep='')
+write.csv(index_out, file='datasets.csv', row.names=FALSE)
 
-# HTML index (TODO: Gets rebuilt every time, so may throw away information if certain datasets disappear from the R package)
+# HTML index 
+index_out$csv = paste("<a href='", index_out$csv, "'> CSV </a>", sep='')
+index_out$doc = paste("<a href='", index_out$doc, "'> DOC </a>", sep='')
 unlink('datasets.html')
-HTML(out, file='datasets.html', row.names=FALSE)
-
-# CSV index
-out$Doc = paste('https://raw.github.com/vincentarelbundock/Rdatasets/master/doc/', 
-                out$Dataset, '.html', sep='')
-out$CSV = paste('https://raw.github.com/vincentarelbundock/Rdatasets/master/csv/', 
-                out$Dataset, '.csv', sep='')
-if(file.exists('datasets.csv')){
-    out2 = read.csv('datasets.csv', col.names=colnames(out))
-    out = unique(rbind(out, out2))
-}
-write.csv(out, file='datasets.csv', row.names=FALSE)
-
-
-
+HTML(index_out, file='datasets.html', row.names=FALSE)
 
